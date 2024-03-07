0727004589f8aeb0df2635d22c6ccc8c3a93a21d
Franklin <71532004+fran1014@users.noreply.github.com>
2024-03-07 19:20:24 +0100

    creating first token contract from scratch

---
+++ contracts/Woox.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Woox {
+    string public name = "Woox";
+    string public symbol = "WOOX";
+    string public standard = "Woox v.0.1";
+    uint256 public totalSupply;
+    address public ownerOfContract;
+    uint256 public _userId;
+    
+    address [] public holderToken;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping (address => TokenHolderInfo) public tokenHolderInfos;
+
+    struct TokenHolderInfo {
+        uint256 _tokenId;
+        address _from;
+        address _to;
+        uint256 _totalToken;
+        bool _tokenHolder;
+    }
+
+    mapping (address => uint256) public balanceOf;
+
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    constructor (uint256 _initialSupply){
+        ownerOfContract = msg.sender;
+        balanceOf [msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    function inc() internal {
+        _userId++;
+    }
+
+    function transfer(address _to, uint256 _value)
+         public 
+         returns (bool success)
+    {
+        require(balanceOf [msg.sender] >= _value);
+        inc();
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        TokenHolderInfo storage tokenHolderInfo = tokenHolderInfos [_to];
+
+        tokenHolderInfo._to = _to;
+        tokenHolderInfo._from = msg.sender;
+        tokenHolderInfo._totalToken = _value;
+        tokenHolderInfo._tokenHolder= true;
+        tokenHolderInfo._tokenId = _userId;
+
+        holderToken.push(_to);
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+
+    } 
+}
