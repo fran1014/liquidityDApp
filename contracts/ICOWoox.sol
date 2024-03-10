a8905b056e973a0d882896b8502c64e0f6b0f5ab
Franklin <71532004+fran1014@users.noreply.github.com>
2024-03-10 21:57:25 +0100

    finish creating ICOWoox token contract

--- contracts/ICOWoox.sol
+++ contracts/ICOWoox.sol
@@ -11,9 +11,40 @@ contract ICOWoox {
 
     event Sell(address _buyer, uint256 _amount);
 
-    constructor (Woox _tokenContract, uint256 _tokenPrice){
+    constructor(Woox _tokenContract, uint256 _tokenPrice) {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    function multiply(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(
+            tokenContract.transfer(
+                msg.sender,
+                _numberOfTokens * 1000000000000000000
+            )
+        );
+
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public {
+        require(msg.sender == admin);
+        require(
+            tokenContract.transfer(
+                admin,
+                tokenContract.balanceOf(address(this))
+            )
+        );
+
+        payable(admin).transfer(address(this).balance);
     }
 }
