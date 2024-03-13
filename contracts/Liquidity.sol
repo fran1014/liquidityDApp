ccd421e502baeaf5be945468d3e78534f7bda894
Franklin <71532004+fran1014@users.noreply.github.com>
2024-03-13 19:48:40 +0100

    creating liquidity contract

---
+++ contracts/Liquidity.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Liquidity {
+    address public admin;
+
+    mapping(address => LiquidityInfo[]) public liquidities;
+    uint256 public liquidityID;
+
+    struct LiquidityInfo {
+        uint256 id;
+        address owner;
+        string tokenA;
+        string tokenB;
+        string tokenA_Address;
+        string tokenB_Address;
+        string poolAddress;
+        string network;
+        string transactionHash;
+        uint256 timeCreated;
+    }
+}
